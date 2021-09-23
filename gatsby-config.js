050fa274c70f9d2857ef708c7e5546741821f0b7
yile-tines <91069006+yile-tines@users.noreply.github.com>
2021-09-23 11:51:02 +0100

    DATO_USE_PREVIEW_API

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
       resolve: "gatsby-source-datocms",
       options: {
         apiToken: process.env.DATO_API_TOKEN,
+        preview: process.env.DATO_USE_PREVIEW_API,
         environment: process.env.DATO_ENVIRONMENT,
       },
     },
